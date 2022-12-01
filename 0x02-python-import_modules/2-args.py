fec7d33d24d22a289634c1a46674610e7bb103c1
Joseph Kakai <josephngalu96gmail.com>
2022-12-01 18:52:20 +0300

    Corrected Error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
 
     argc = len(sys.argv) - 1
     if argc == 0:
-        print("0 argument")
+        print("0 arguments.")
     elif argc == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
-        print("{} arguments".format(argc))
+        print("{} arguments:".format(argc))
     for i in range(argc):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
