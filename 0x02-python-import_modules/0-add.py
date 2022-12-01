ebb6101447b3a522058c99a3aa3d91746922d044
Joseph Kakai <josephngalu96gmail.com>
2022-12-01 17:55:12 +0300

    Corrected pycodestyle error

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Print sum of a and b"""
     from add_0 import add
-    a, b = 1, 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    a = 1 
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
