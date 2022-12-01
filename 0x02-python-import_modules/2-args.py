8a0340301f19a9910171d135cf0c1279c3424045
Joseph Kakai <josephngalu96gmail.com>
2022-12-01 21:58:59 +0300

    f

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print number of arguments and arguments"""
+    """Print number of arguments  and arguments"""
     import sys
 
     argc = len(sys.argv) - 1
