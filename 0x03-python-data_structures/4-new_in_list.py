ceca8480e0c1ef295a96cfc57be70e820e5fd505
Joseph Kakai <josephngalu96gmail.com>
2022-12-05 02:41:04 +0300

    Added task 5

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if my_list:
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        else:
+            cyp_list = my_list.copy()
+            my_list[idx] = element
+            return cyp_list
