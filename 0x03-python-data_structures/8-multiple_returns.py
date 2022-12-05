92f1ab732c2e6b2fbb1cb6ffba90f233e98e2a5b
Joseph Kakai <josephngalu96gmail.com>
2022-12-05 21:37:03 +0300

    Corrected error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    first_char= sentence[0] 
+    first_char = sentence[0]
     num_of_chars = len(sentence)
-
     if num_of_chars == 0:
         first_char = "None"
     return num_of_chars, first_char
