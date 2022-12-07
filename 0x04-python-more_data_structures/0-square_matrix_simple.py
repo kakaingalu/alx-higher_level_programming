f2c629e56049d5a708d385e35fa82e896de8269f
Joseph Kakai <josephngalu96gmail.com>
2022-12-07 20:45:53 +0300

    Corrected

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
         row_matrix = []
-        for elements in row:
-            row_matrix.append(element**)
+        for element in row:
+            row_matrix.append(element**2)
         new_matrix.append(row_matrix)
     return new_matrix
