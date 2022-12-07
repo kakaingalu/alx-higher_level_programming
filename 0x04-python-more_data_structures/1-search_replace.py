899da50b19341ff8ea5188172e44bb82db8363d8
Joseph Kakai <josephngalu96gmail.com>
2022-12-07 20:39:47 +0300

    Added all files

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for i in my_list:
+        if i == search
+            i = replace
+            
+        new_list.append(i)
+        return new_list
