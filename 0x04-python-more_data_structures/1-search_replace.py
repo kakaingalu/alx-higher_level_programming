20e58a6bd11e99484fb9daffb9052855c2fb5377
Joseph Kakai <josephngalu96gmail.com>
2022-12-07 20:42:51 +0300

    Corrected all files

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for i in my_list:
-        if i == search
+        if i == search:
             i = replace
-            
         new_list.append(i)
         return new_list
