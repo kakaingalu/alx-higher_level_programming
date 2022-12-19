4ed69d004273fed2177a18bbd59769150ae9860c
Joseph Kakai <josephngalu96gmail.com>
2022-12-19 16:38:13 +0300

    Corrected pycode style error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     counter = 0
     for i in range(x):
         try:
-         print("{}".format(my_list[i]), end='')
-         counter += 1
+            print("{}".format(my_list[i]), end='')
+            counter += 1
         except IndexError:
             pass
     print()
