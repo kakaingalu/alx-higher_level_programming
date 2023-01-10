4348d6272cec230d888a291d50dd960d3f5c7abb
KAKAIRICHARD <josephngalu96@gmail.com>
2023-01-10 12:33:45 -0800

    complete tasks

---
+++ 0x0B-python-input_output/10-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+Student = __import__('10-student').Student
+
+student_1 = Student("John", "Doe", 23)
+student_2 = Student("Bob", "Dylan", 27)
+
+j_student_1 = student_1.to_json()
+j_student_2 = student_2.to_json(['first_name', 'age'])
+j_student_3 = student_2.to_json(['middle_name', 'age'])
+
+print(j_student_1)
+print(j_student_2)
+print(j_student_3)
+
+guillaume@ubuntu:~/0x0B$ ./10-main.py 
+{'age': 23, 'last_name': 'Doe', 'first_name': 'John'}
+{'age': 27, 'first_name': 'Bob'}
+{'age': 27}
