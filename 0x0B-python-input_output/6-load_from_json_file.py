a0bccd8ac41a08b40bd62e4f3a88c5ea0b1f79a5
KAKAIRICHARD <josephngalu96@gmail.com>
2023-01-10 04:47:14 -0800

    corrcted error

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a JSON file-writing function"""
+"""This module defines a JSON file-reading function"""
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file using JSON format"""
-    with open(filename, "w") as f:
-        json.dump(my_obj, f)
+def load_from_json_file(filename):
+    """Creates a Python object from a given JSON file"""
+    with open(filename) as f:
+        return json.load(f)
