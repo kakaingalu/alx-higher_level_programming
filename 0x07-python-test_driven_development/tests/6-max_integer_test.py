ac59bade7ce342800aa5bc4b3afc83624a9bfa87
Joseph Kakai <49359467+josephkakai@users.noreply.github.com>
2023-02-12 20:59:42 +0300

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,37 +1,26 @@
 #!/usr/bin/python3
-"""Unittests for max_integer([..])."""
-
+"""Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Define unittests for max_integer([..])."""
+    def test_max_value(self):
+        self.assertEqual(max_integer([5, 3, 2]), 5)
+        self.assertEqual(max_integer([4, 6, 7]), 7)
+        self.assertEqual(max_integer([4, 8, 5]), 8)
+        self.assertEqual(max_integer([4, 6, -2]), 6)
+        self.assertEqual(max_integer([-4, -5, -8]), -4)
+        self.assertEqual(max_integer([-4]), -4)
+        self.assertEqual(max_integer([5]), 5)
 
-    def test_ordered_list(self):
-        """Test an ordered list of integers."""
-        ordered = [1, 2, 3, 4]
-        self.assertEqual(max_integer(ordered), 4)
+    def test_empty(self):
+        self.assertIsNone(max_integer([]))
 
-    def test_unordered_list(self):
-        """Test an unordered list of integers."""
-        unordered = [1, 2, 4, 3]
-        self.assertEqual(max_integer(unordered), 4)
 
-    def test_max_at_begginning(self):
-        """Test a list with a beginning max value."""
-        max_at_beginning = [4, 3, 2, 1]
-        self.assertEqual(max_integer(max_at_beginning), 4)
-
-    def test_empty_list(self):
-        """Test an empty list."""
-        empty = []
-        self.assertEqual(max_integer(empty), None)
-
-    def test_one_element_list(self):
-        """Test a list with a single element."""
-        one_element = [7]
-        self.assertEqual(max_integer(one_element), 7)
+if __name__ == "__main__":
+    unittest.main()
 
     def test_floats(self):
         """Test a list of floats."""
