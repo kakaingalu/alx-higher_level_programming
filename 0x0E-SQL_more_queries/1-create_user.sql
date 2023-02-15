e185b34b45bfadca689cbc22e974e0c1ef472b6d
Joseph Kakai <josephngalu96@gmail.com>
2023-02-15 09:55:14 -0800

    Updated error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- a script that creates the MySQL server user user_0d_1.
-CREATE USER IF NOT EXISTS 'user_id_1'@'localhost' IDENTIFIED BY 'user_od_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_od_1'@'localhost';
+-- create and grant permissions to a user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
