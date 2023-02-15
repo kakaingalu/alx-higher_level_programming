d8e3277956af5b5b125335f4e6fada83dd331296
Joseph Kakai <josephngalu96@gmail.com>
2023-02-15 12:31:52 -0800

    updated

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
-mysql -u root -p hbtn_0d_tvshows -e "title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;"
+mysql -u root -p hbtn_0d_tvshows -e "title, genre_id FROM tv_shows INNER JOIN tv_show_genres ON id = tv_show_id ORDER BY title ASC, genre_id ASC;"
 
