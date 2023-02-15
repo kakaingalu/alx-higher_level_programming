fa753cf56ec2254b352d295b5796167eaa07435a
Joseph Kakai <josephngalu96@gmail.com>
2023-02-15 09:49:17 -0800

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- a script that creates the MySQL server user user_0d_1.
 CREATE USER IF NOT EXISTS 'user_id_1'@'localhost' IDENTIFIED BY 'user_od_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_od_1'@'localhost';
-FLUSH PRIVILEGES;
