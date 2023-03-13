422fbfb96579bd9c0279acae323ce1414bbb2581
Joseph Kakai <Josephngalu96@gmail.com>
2023-03-13 17:53:01 +0000

    created 6-multi_language_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// a script that prints 3 lines
+const print = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (print[i]) {
+  console.log(print[i]);
+  i++;
+}
