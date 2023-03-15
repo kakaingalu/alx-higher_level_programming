bf8b8288de95e74cb31288691e9e64d5252b40d7
Wanton Raven <49359467+wantonraven@users.noreply.github.com>
2023-03-15 19:59:20 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// a script that prints 3 lines
+// a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop.
 const print = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (print[i]) {
