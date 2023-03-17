f983ca6a1efc86af8f45c341edf3b34a66d68fad
Joseph Kakai <josephngalu96@gmail.com>
2023-03-17 05:19:07 -0700

    created 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+ a script that lists all states with a name starting \
+         with N (upper N) from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Get data from database
+    """
+    states = argv[4]
+    db_conct = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3],)
+
+    cursor = db_conct.cursor()
+
+    cursor.execute(f"SELECT * FROM states WHERE name LIKE BINARY '{states}' \
+            ORDER BY states.id ASC")
+
+    states_name = cursor.fetchall()
+
+    for state in states_name:
+        print(state)
+
+    cursor.close()
+    db_conct.close()
