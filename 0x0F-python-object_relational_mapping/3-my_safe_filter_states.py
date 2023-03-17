291102b2af20b77a2d3df80e374a1201666b16d1
Joseph Kakai <josephngalu96@gmail.com>
2023-03-17 06:51:16 -0700

    updated the error finally

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,8 +11,9 @@ if __name__ == "__main__":
     Get data from database
     """
 
-    db_conct = MySQLdb.connect(host="localhost", user=argv[1],\
-            port=3306, passwd=argv[2], db=argv[3], charset="utf8")
+    db_conct = MySQLdb.connect(host="localhost", user=argv[1],
+                               port=3306, passwd=argv[2],
+                               db=argv[3], charset="utf8")
 
     cursor = db_conct.cursor()
     query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
