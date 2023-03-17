a76b19caf80356d7fa2e61c4816c0e723da7e054
Joseph Kakai <josephngalu96@gmail.com>
2023-03-17 07:18:14 -0700

    corrected pycode error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -16,7 +16,9 @@ if __name__ == "__main__":
                                db=argv[3], charset="utf8")
 
     cursor = db_conct.cursor()
-    query = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    query = "SELECT cities.id, cities.name, states.name FROM cities\
+            INNER JOIN states ON cities.state_id = states.id ORDER\
+            BY cities.id ASC"
     cursor.execute(query)
 
     states_name = cursor.fetchall()
