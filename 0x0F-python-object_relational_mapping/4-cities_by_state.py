bda666330c7a8f928f1a5fe3f74d6b65c2201b51
Joseph Kakai <josephngalu96@gmail.com>
2023-03-17 07:15:47 -0700

    created 4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+ a script that lists all states with a name starting \
+         with N (upper N) from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Get data from database
+    """
+
+    db_conct = MySQLdb.connect(host="localhost", user=argv[1],
+                               port=3306, passwd=argv[2],
+                               db=argv[3], charset="utf8")
+
+    cursor = db_conct.cursor()
+    query = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    cursor.execute(query)
+
+    states_name = cursor.fetchall()
+
+    for state in states_name:
+        print(state)
+
+    cursor.close()
+    db_conct.close()
