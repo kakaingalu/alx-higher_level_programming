2bee14f2e6a50ee5e744f576cac378984ae84af2
Joseph Kakai <josephngalu96@gmail.com>
2023-03-17 10:25:17 -0700

    Corrected syntax error

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,9 +10,11 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    """
+    access the datbase and get states
+    """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metdata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
@@ -20,4 +22,4 @@ if __name__ == "__main__":
     states = session.query(State).order_by(State.id).all()
 
     for state in states:
-        print("{}:{}".format(state.id, state.name))
+        print("{0}:{1}".format(state.id, state.name))
