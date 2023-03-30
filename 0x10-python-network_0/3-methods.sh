b98ab5f6f5bb708f5f58ae2eb178c9bfb5422811
Wanton Raven <josephngalu96@gmail.com>
2023-03-30 14:17:13 -0700

    created 6-peak.txt and updated 3-methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sXI OPTIONS "$1" -v |& awk '/^Allow:/ {print $2}'
+curl -sXI OPTIONS "$1" -o /dev/null -w "%{allow}\n'
+
