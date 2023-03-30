35d567f4d179b16c659e86adc18003e445a554f9
Wanton Raven <josephngalu96@gmail.com>
2023-03-30 14:25:25 -0700

    modified 6-peak code and 3-methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sXI OPTIONS "$1" -o /dev/null -w "%{allow}\n'
-
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,14 +4,15 @@
 def find_peak(list_of_integers):
     if not list_of_integers:
         return None
-
-    left, right = 0, len(list_of_integers) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-
-    return list_of_integers[left]
+    n = len(list_of_integers)
+    if n == 1:
+        return list_of_integers[0]
+    if n == 2:
+        return max(list_of_integers)
+    mid = n // 2
+    if list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    elif list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid + 1:])
+    else:
+        return list_of_integers[mid]
