1d593550a5c707c3f4bc9120cd2943e9cd01aea6
Wanton Raven <josephngalu96@gmail.com>
2023-03-31 13:31:20 -0700

    corrected pycodestyle error on task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import urllib.request
-from sys import argv
-import urllib.parse
 """ a Python script that takes in a URL and an email,\
         sends a POST request to the passed URL with the\
         email as a parameter, and displays the body \
         of the response (decoded in utf-8) """
+import urllib.request
+from sys import argv
+import urllib.parse
 
 if __name__ == "__main__":
     url = argv[1]
