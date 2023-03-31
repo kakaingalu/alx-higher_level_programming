16f7f2431ec82e7cd797292e6559ea14edd4f1be
Wanton Raven <josephngalu96@gmail.com>
2023-03-31 16:46:19 -0700

    corrected pycode error on task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,6 +5,8 @@
 import requests
 import sys
 if __name__ == "__main__":
-    response = requests.get('https://api.github.com/user', headers={ 'Accept': 'application/vnd.github.v3+json'}, auth=(sys.argv[1], sys.argv[2]))
+    response = requests.get('https://api.github.com/user',
+                            headers={'Accept': 'application/vnd.github.v3+json'
+                                     }, auth=(sys.argv[1], sys.argv[2]))
     json_dict = response.json()
     print(json_dict.get('id') if response.status_code == 200 else None)
