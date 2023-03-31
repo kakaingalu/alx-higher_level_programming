d6361c27c557a75210ab1000076464c06d9cd69f
Wanton Raven <josephngalu96@gmail.com>
2023-03-31 14:00:24 -0700

    updated task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)."""
+""" a Python script that takes in a URL,\
+        sends a request to the URL and \
+        displays the body of the response\
+        (decoded in utf-8)."""
+
 import urllib.request
 from sys import argv
 import urllib.error
 
 if __name__ == "__main__":
     url = argv[1]
-    
+
     try:
         with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
