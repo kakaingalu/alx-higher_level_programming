0dffce0984d7282d4067a9d856519fa30c041317
Wanton Raven <josephngalu96@gmail.com>
2023-03-31 16:36:38 -0700

    created task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends arequest to\
+        the URL and displays the body of the response."""
+
+import requests
+import sys
+if __name__ == "__main__":
+    response = requests.get('https://api.github.com/user', headers={ 'Accept': 'application/vnd.github.v3+json'}, auth=(sys.argv[1], sys.argv[2]))
+    json_dict = response.json()
+    print(json_dict.get('id') if response.status_code == 200 else None)
