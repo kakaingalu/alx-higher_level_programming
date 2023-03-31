0b1d28839295621ecc388e3c31aec28c5a842d28
Wanton Raven <josephngalu96@gmail.com>
2023-03-31 13:09:03 -0700

    updated error on task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 import urllib.request
 """ a Python script that fetches https://alx-intranet.hbtn.io/status """
-
-
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    if response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- uft8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        if response:
+            content = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- uft8 content: {}".format(content.decode('utf-8')))
