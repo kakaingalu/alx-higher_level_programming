7678efe53c721c357c4329ee2dc47133ea6e3b16
Joseph Kakai <josephngalu96@gmail.com>
2023-04-04 05:14:03 -0700

    updated logic on peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,5 +4,17 @@
 
 def find_peak(list_of_integers):
     """ Checks if the list has any value if so it returns max"""
-    if list_of_integers:
-        return max(list_of_integers) 
+    # check if list has any value
+    if not list_of_integers:
+        return None
+
+    # initialize left to 0 and right the length of list - 1
+    left, right = 0, len(list_of_integers) - 1
+    # loop through the list using its length
+    while left < right:
+        peak = (left + right) // 2  # peak is initialized by half of length
+        if list_of_integers[peak] < list_of_integers[peak + 1]:
+            left = peak + 1
+        else:
+            right = peak
+    return list_of_integers[left]
