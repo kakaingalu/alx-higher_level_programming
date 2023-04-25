4261705cd0f432ae9a80cde12fe5ea0aece49821
Joseph Kakai <josephngalu96@gmail.com>
2023-04-25 07:34:19 -0700

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 // a script that reads and prints the content of a file
 
 const fs = require('fs');
-const filePath = process.argv[2];
-fs.readFile(filePath, 'utf-8', (err, data) => {
+const fileP = process.argv[2];
+fs.readFile(fileP, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
     return;
