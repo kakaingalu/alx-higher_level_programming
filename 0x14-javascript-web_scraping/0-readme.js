3e751b4924c723e5623cb1f8f1de5abf29d84596
Joseph Kakai <josephngalu96@gmail.com>
2023-04-25 07:26:20 -0700

    created 0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// a script that reads and prints the content of a file
+
+const fs = require('fs');
+const filePath = process.argv[2];
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
