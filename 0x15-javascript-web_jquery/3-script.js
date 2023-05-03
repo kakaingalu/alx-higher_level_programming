11da4d2e50b3e503054be70a6a2df100a95fd61b
Wanton Raven <josephngalu96@gmail.com>
2023-05-03 21:11:00 +0300

    Update 3-script.js

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,6 +1,3 @@
-#!/usr/bin/node
-//a JavaScript script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header
-
 $(document).ready($('DIV#red_header').click(() => {
   $('header').addClass('red')
 }));
