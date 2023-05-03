28d2ff495912dde6e14ecf9d9f3d29bafd3775f8
Wanton Raven <josephngalu96@gmail.com>
2023-05-03 21:09:06 +0300

    Update 7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,3 @@
-#!/usr/bin/node
-//  a JavaScript script that fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
-
 $(document).ready(() => {
   $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', (myData) => {
   $('DIV#character').text(myData.name)
