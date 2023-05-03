07fe17091bdbd4c7f03b6acf3a773bf959349cdf
Wanton Raven <josephngalu96@gmail.com>
2023-05-03 21:10:36 +0300

    Update 4-script.js

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,6 +1,3 @@
-#!/usr/bin/node
-//a JavaScript script that toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header
-
 $(document).ready($('DIV#toggle_header').click(() => {
   $('header').toggleClass('red green')
 }));
