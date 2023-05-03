a5d31a5a3b9ab3bbf5d2fe5f015dc29b4d4ac142
Wanton Raven <josephngalu96@gmail.com>
2023-05-03 21:09:57 +0300

    Update 5-script.js

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,6 +1,3 @@
-#!/usr/bin/node
-// a JavaScript script that adds a <li> element to a list when the user clicks on the tag DIV#add_item
-
 $(document).ready($('DIV#add_item').click(() => {
   $('UL.my_list').append('<li>Item</li>')
 }));
