7df81782f558c621204c2fadb22a3d453bedc5b7
Wanton Raven <josephngalu96@gmail.com>
2023-05-03 21:08:44 +0300

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,6 +1,3 @@
-#!/usr/bin/node
-//a JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.alx-tools.com/api/films/?format=json
-
 $(document).ready(() => {
   $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (myData) {
     $.each(myData.results, function (i, film) {
